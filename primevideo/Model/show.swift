63406a36368460e69049fa605fb19734d34144ce
Giuseppe Casillo <g.casillox2@gmail.com>
2023-11-14 10:23:10 +0100

    Created the class Show (no enum, the purpose is not create a perfect class)

--- primevideo/Model/show.swift
+++ primevideo/Model/show.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+class Show: Identifiable{
+    let id: UUID
+    private let type: String
+    private let name: String
+    private let cover: String
+    private let original: Bool
+    private let genres: [String]
+    
+    init(type: String, name: String, cover: String, original: Bool, genres: [String]) {
+        self.id = UUID()
+        self.type = type
+        self.name = name
+        self.cover = cover
+        self.original = original
+        self.genres = genres
+    }
+    
+}
