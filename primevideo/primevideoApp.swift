59ccdd2da3374df9ecd9803319db633983871837
Giuseppe Casillo <g.casillox2@gmail.com>
2023-11-14 10:09:01 +0100

    Initial Commit

---
+++ primevideo/primevideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  primevideoApp.swift
+//  primevideo
+//
+//  Created by Giuseppe Casillo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct primevideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
