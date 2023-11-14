b9c52e8fe6648b25989562ec81467febb0f67ed5
Giuseppe Casillo <g.casillox2@gmail.com>
2023-11-14 19:43:59 +0100

    Added top-right buttons

--- primevideo/View/ContentView.swift
+++ primevideo/View/ContentView.swift
@@ -10,41 +10,51 @@ import SwiftUI
 struct ContentView: View {
     @State private var selectedCategory = "all"
     var body: some View {
+        ScrollView{
             
-            ScrollView{
-                HStack {
-                    Image("prime_logo").resizable().frame(width: 100, height: 60).colorInvert()
-                    Spacer()
-                }
+            HStack {
+                Image("prime_logo").resizable().frame(width: 120, height: 70).colorInvert()
+                Spacer()
                 HStack{
-                    Button(action: {
-                        selectedCategory = "all"
-                    }, label: {
-                        Text("All").frame(width: 50, height: 30)
-                    }).background(selectedCategory=="all" ? .white : .black).foregroundStyle(selectedCategory=="all" ? .black : .white).clipShape(Capsule())
-                    
-                    Button(action: {
-                        selectedCategory = "film"
-                    }, label: {
-                        Text("Film").frame(width: 50, height: 30)
-                    }).background(selectedCategory=="film" ? .white : .black).foregroundStyle(selectedCategory=="film" ? .black : .white).clipShape(Capsule())
-                    
-                    Button(action: {
-                        selectedCategory = "series"
-                    }, label: {
-                        Text("TV-Series").frame(width: 90, height: 30)
-                    }).background(selectedCategory=="series" ? .white : .black).foregroundStyle(selectedCategory=="series" ? .black : .white).clipShape(Capsule())
-                    
-                    
-                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "tv.badge.wifi").foregroundStyle(.white)
+                    })
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Circle().frame(width: 30, height: 30)
+                    })
                 }.padding(.horizontal)
-                originalCarouselView()
-          
+                
+            }
+            HStack{
+                Button(action: {
+                    selectedCategory = "all"
+                }, label: {
+                    Text("All").frame(width: 50, height: 30)
+                }).background(selectedCategory=="all" ? .white : .black).foregroundStyle(selectedCategory=="all" ? .black : .white).clipShape(Capsule())
+                
+                Button(action: {
+                    selectedCategory = "film"
+                }, label: {
+                    Text("Film").frame(width: 50, height: 30)
+                }).background(selectedCategory=="film" ? .white : .black).foregroundStyle(selectedCategory=="film" ? .black : .white).clipShape(Capsule())
+                
+                Button(action: {
+                    selectedCategory = "series"
+                }, label: {
+                    Text("TV-Series").frame(width: 90, height: 30)
+                }).background(selectedCategory=="series" ? .white : .black).foregroundStyle(selectedCategory=="series" ? .black : .white).clipShape(Capsule())
+                
+                
+                Spacer()
+            }.padding(.horizontal)
+            originalCarouselView()
+            
         }.preferredColorScheme(.dark)
             .ignoresSafeArea()
         
+    }
 }
-}
+
 
 #Preview {
     ContentView()
